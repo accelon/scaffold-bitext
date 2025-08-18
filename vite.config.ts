623b8ff82ae3c6a4e0892db5f236ca379086fe5e
yapcheahshen <yapcheahshen@gmail.com>
2025-08-18 16:17:55 +0800

    server port 3000, no asset name mangling

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vite.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+         assetFileNames: '[name][extname]',
+        entryFileNames: '[name].js', // Custom path for entry files
+        chunkFileNames: 'chunks/[name].js', // Custom path for chunk files
+      },
+    },
+  },    
+  plugins: [svelte()],
+  server: {
+    port: 3000, // Change to your desired port
+  }
+})

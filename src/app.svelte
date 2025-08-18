1363f19609245c8b706b0b54ad571b4313c5897b
yapcheahshen <yapcheahshen@gmail.com>
2025-08-18 17:45:12 +0800

    code mirror ok

--- src/app.svelte
+++ src/app.svelte
@@ -1,47 +1,33 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/counter.svelte'
+  // import Counter from './lib/counter.svelte'
+  import ToolbarLeft from './toolbarleft.svelte'
+  import ToolbarRight from './toolbarright.svelte'
+  import SplitPane from './lib/splitpane.svelte';
+  import Codemirror from './codemirror.svelte';
+  let pos=45;
+  let value1=$state('abcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+  let value2=$state('xyz')
+  const editorstyle={
+          height:"95vh",
+
+        }
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+<SplitPane type="horizontal" bind:pos min={15} max={85}>
+  
+    <div slot="a">
+      <ToolbarLeft/>
+        <Codemirror bind:value={value1} lineWrapping={true} styles={{"&": editorstyle }}/>
+    </div>
+    <div slot="b">
+      <ToolbarRight/>
+        <Codemirror bind:value={value2} styles={{"&": editorstyle }}/>
+    </div>
+</SplitPane>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  main {width:100vw;height:100vh}
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
       },
     },
   },    
+  optimizeDeps: {
+        exclude: [ "codemirror" /* ... */],
+  },  
   plugins: [svelte()],
   server: {
     port: 3000, // Change to your desired port
